e1a9e1136be546882d03ad2ab7a2673dca574895
Hosein-Ahmadzade <64315919+Hosein-Ahmadzade@users.noreply.github.com>
2021-04-15 14:24:56 +0430

    home and name routes added to route.py file

--- application/routes.py
+++ application/routes.py
@@ -7,3 +7,15 @@ from flask import render_template
 
 def index():
     return render_template('index.html')
+
+
+@app.route("/home")
+
+def home():
+    return "<h1>This is my Home Page :)</h1>"
+
+
+@app.route("/<name>")
+
+def user(name):
+    return f"Hello {name}"
